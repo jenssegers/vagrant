23446ab7b11dccaab4b8c3848d8229fdfe21fc23
Jens Segers <segers.jens@gmail.com>
2015-08-17 20:36:28 +0200

    Add homestead db user

--- site-cookbooks/custom/recipes/databases.rb
+++ site-cookbooks/custom/recipes/databases.rb
@@ -19,6 +19,13 @@ mysql_database_user "root" do
     action :grant
 end
 
+mysql_database_user "homestead" do
+    connection mysql_connection_info
+    password "secret"
+    host '%'
+    action [:create, :grant]
+end
+
 # --------------------------------------
 # Databases
 # --------------------------------------
