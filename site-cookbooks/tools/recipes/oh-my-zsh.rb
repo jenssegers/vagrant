83932ad23d424d8569a716e96879b1e3ae2ebc2d
Jens Segers <segers.jens@gmail.com>
2015-10-11 12:13:40 +0200

    Update oh-my-zsh

--- site-cookbooks/tools/recipes/oh-my-zsh.rb
+++ site-cookbooks/tools/recipes/oh-my-zsh.rb
@@ -13,7 +13,17 @@ remote_file "/home/vagrant/.oh-my-zsh/themes/jenssegers.zsh-theme" do
 end
 
 remote_file "/home/vagrant/.zshrc" do
-    source "https://raw.githubusercontent.com/jenssegers/dotfiles/master/.zshrc"
+	source "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/templates/zshrc.zsh-template"
+end
+
+execute "change-theme" do
+	command "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"jenssegers\"/g' /home/vagrant/.zshrc"
+	only_if "grep 'ZSH_THEME=\"robbyrussell\"' /home/vagrant/.zshrc"
+end
+
+execute "disable-auto-update" do
+	command "sed -i 's/# DISABLE_AUTO_UPDATE=\"true\"/DISABLE_AUTO_UPDATE=\"true\"/g' /home/vagrant/.zshrc"
+	only_if "grep '# DISABLE_AUTO_UPDATE=\"true\"' /home/vagrant/.zshrc"
 end
 
 user "vagrant" do
